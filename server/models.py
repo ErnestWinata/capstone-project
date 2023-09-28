a11b5ece2cda807c161b7b485a4a53bf6008adaf
Ernest <ernestwinata@gmail.com>
2023-09-28 09:20:46 -0700

    define User class

--- server/models.py
+++ server/models.py
@@ -3,4 +3,13 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    cities = db.relationship('City', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
