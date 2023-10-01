c15515648182f2c42146b87cac7457a1ac8da8dc
Ernest <ernestwinata@gmail.com>
2023-09-30 17:34:25 -0700

    make api request

--- client/src/components/CityList.js
+++ client/src/components/CityList.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function CityList() {
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/cities')  // Replace with your API URL
+      .then(response => response.json())
+      .then(data => setCities(data))
+      .catch(error => console.error('Error fetching cities:', error));
+  }, []);
+
   return (
     <div>
       <h1>List of Cities</h1>
       <ul>
-        <li>New York</li>
-        <li>London</li>
-        <li>Tokyo</li>
+        {cities.map(city => (
+          <li key={city.id}>{city.name}</li>
+        ))}
       </ul>
     </div>
   );
 }
 
 export default CityList;
+
