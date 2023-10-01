f49438c09f43ddfba31f603da06b5d69554ef91d
Ernest <ernestwinata@gmail.com>
2023-09-30 17:39:15 -0700

    set up Formik

---
+++ client/src/components/LogInForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required'),
+});
+
+function LoginForm() {
+  const handleSubmit = (values, { setSubmitting }) => {
+    console.log('Form values:', values);
+    setSubmitting(false);
+  };
+
+  return (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={LoginSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <label htmlFor="email">Email</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+          </div>
+
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default LoginForm;
