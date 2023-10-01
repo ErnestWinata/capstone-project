de7e5486e53ff8f88e8625c183c24c6adb8336fb
Ernest <ernestwinata@gmail.com>
2023-09-30 17:31:23 -0700

    implement React components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+// Import necessary modules
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CityList from './components/CityList';
+import CityDetails from './components/CityDetails';
+import LoginForm from './components/LoginForm';
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <Router>
+      <div>
+        {/* Define routes */}
+        <Switch>
+          <Route path="/cities" component={CityList} />
+          <Route path="/city/:id" component={CityDetails} />
+          <Route path="/login" component={LoginForm} />
+          {/* Add more routes as needed */}
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/CityList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function CityList() {
+  return (
+    <div>
+      <h1>List of Cities</h1>
+      <ul>
+        <li>New York</li>
+        <li>London</li>
+        <li>Tokyo</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CityList;
