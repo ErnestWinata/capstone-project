5cd1aa27d0859a08bbc909cb08ab5c4ce263d1b4
Ernest <ernestwinata@gmail.com>
2023-10-02 17:29:20 -0700

    create handles

--- server/app.py
+++ server/app.py
@@ -1,27 +1,37 @@
-#!/usr/bin/env python3
-
-
 from flask import request, jsonify
 from models import db, City
-from config import app, api
-
-
 
 @app.route('/cities', methods=['POST'])
 def create_city():
-    pass
+    data = request.json
+    new_city = City(name=data['name'], date_of_visit=data['date_of_visit'],
+                    best_memories=data['best_memories'], accomodation=data['accomodation'],
+                    user_id=data['user_id'])
+    db.session.add(new_city)
+    db.session.commit()
+    return jsonify({'message': 'City created successfully'}), 201
 
 @app.route('/cities', methods=['GET'])
 def get_all_cities():
-    pass
+    cities = City.query.all()
+    serialized_cities = [city.serialize() for city in cities]  # Assuming a serialize method in your City model
+    return jsonify(serialized_cities), 200
 
 @app.route('/cities/<int:city_id>', methods=['GET'])
 def get_city(city_id):
-    pass
+    city = City.query.get(city_id)
+    if not city:
+        return jsonify({'message': 'City not found'}), 404
+    return jsonify(city), 200
 
 @app.route('/cities/<int:city_id>', methods=['DELETE'])
 def delete_city(city_id):
-    pass
+    city = City.query.get(city_id)
+    if not city:
+        return jsonify({'message': 'City not found'}), 404
+    db.session.delete(city)
+    db.session.commit()
+    return jsonify({'message': 'City deleted successfully'}), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
