2c08e50864a800e74669bea4e6f03b8bfcc79ec2
Ernest <ernestwinata@gmail.com>
2023-10-02 17:19:58 -0700

    add error handling in cityList

--- client/src/components/CityList.js
+++ client/src/components/CityList.js
@@ -2,22 +2,37 @@ import React, { useState, useEffect } from 'react';
 
 function CityList() {
   const [cities, setCities] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:5000/cities')  // Replace with your API URL
-      .then(response => response.json())
+    fetch('http://localhost:5000/cities')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch cities');
+        }
+        return response.json();
+      })
       .then(data => setCities(data))
-      .catch(error => console.error('Error fetching cities:', error));
+      .catch(error => {
+        setError(error.message);
+        console.error('Error fetching cities:', error);
+      });
   }, []);
 
   return (
     <div>
-      <h1>List of Cities</h1>
-      <ul>
-        {cities.map(city => (
-          <li key={city.id}>{city.name}</li>
-        ))}
-      </ul>
+      {error ? (
+        <div>Error: {error}</div>
+      ) : (
+        <>
+          <h1>List of Cities</h1>
+          <ul>
+            {cities.map(city => (
+              <li key={city.id}>{city.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
